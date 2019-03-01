dc47dd15220941c64fe132b8d8ed2acda7a31a65
Tim Mandel <timothy.mandel@gmail.com>
2019-03-01 12:22:59 -0600

    final css tweaks made

--- react-bookmatch/src/EditUser/index.js
+++ react-bookmatch/src/EditUser/index.js
@@ -3,10 +3,12 @@ import { withRouter } from 'react-router-dom';
 
 const EditUser = (props)=>{
     return(
-        <form onSubmit={props.editUser.bind(null, props.newUsername)}>
-        <input type="text" name="username" value={props.newUsername} onChange={props.handleEditInput}/>
-        <input type="submit" value="Edit your username"/>
-        </form>
+    	<div className="searchbar">
+	        <form className="login-form" onSubmit={props.editUser.bind(null, props.newUsername)}>
+		        <input className="login-input" type="text" name="username" value={props.newUsername} onChange={props.handleEditInput}/>
+		        <button>Edit Your Username</button>
+	        </form>
+	    </div>
     )
 }
 export default withRouter(EditUser);
